9a3c8ac3e6099ed3e4548ac6d2e6432fbec3cabf
noway <daviddavide847@gmail.com>
2025-04-12 20:08:36 +0200

    Added Client Website

--- Server/esp32-rc-car/app/GoogleAuthProvider.tsx
+++
@@ -1,9 +0,0 @@
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import React, { ReactNode } from "react";
-
-const clientId = "919763154480-u2q39e7vubr5cl5smth8ciqeodnpq6io.apps.googleusercontent.com"; // Sostituisci con il tuo Client ID
-
-// Il provider di autenticazione Google
-export function GoogleAuthProvider({ children }: { children: ReactNode }) {
-  return <GoogleOAuthProvider clientId={clientId}>{children}</GoogleOAuthProvider>; //fornisce il clientId a tutti i componenti figli
-}
--- cyberrover/src/utils/trpc.ts
+++
@@ -1,8 +0,0 @@
-import { createTRPCReact } from '@trpc/react-query';
-import type { AppRouter } from '../trpc/server';
-
-export const trpc = createTRPCReact<AppRouter>();
-
-export const trpcClient = trpc.createClient({
-  url: 'http://localhost:4000',
-});
